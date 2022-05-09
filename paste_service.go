aadd5ac33db10d1e6c1313cc8d1a52971b980b73
Jacob Smith <jsmithdenverdev@gmail.com>
2022-05-09 16:00:16 -0600

    Added go doc comments

--- paste_service.go
+++ paste_service.go
@@ -20,6 +20,7 @@ func NewPasteService(db *gorm.DB, infoLog, errorLog *log.Logger) *PasteService {
 	}
 }
 
+// Create valiates the incoming Paste before persisting it to the datastore.
 func (p *PasteService) Create(paste Paste) (int, error) {
 	if paste.Expiration == "" {
 		return 0, ErrorUnrecognizedExpiration{}
@@ -38,6 +39,9 @@ func (p *PasteService) Create(paste Paste) (int, error) {
 	return paste.ID, nil
 }
 
+// Read finds a Paste with the given id in the datastore. If the Paste has
+// expired it is expunged and an ErrorPasteExpired error is returned to the
+// caller.
 func (p *PasteService) Read(id int) (Paste, error) {
 	paste := Paste{}
 
@@ -57,6 +61,8 @@ func (p *PasteService) Read(id int) (Paste, error) {
 	return paste, nil
 }
 
+// Delete deletes a Paste with the given id from the datastore. If an error
+// occurs it is returned to the caller.
 func (p *PasteService) Delete(id int) error {
 	if tx := p.db.Delete(&Paste{}, id); tx.Error != nil {
 		return tx.Error
@@ -65,7 +71,10 @@ func (p *PasteService) Delete(id int) error {
 	return nil
 }
 
+// List returns all the Paste's in the datastore. Any Paste's that have expired
+// are deleted from the datastore and expunged from the result set.
 func (p *PasteService) List() ([]Paste, error) {
+	pastesRet := []Paste{}
 	pastes := []Paste{}
 
 	if tx := p.db.Find(&pastes); tx.Error != nil {
@@ -74,11 +83,13 @@ func (p *PasteService) List() ([]Paste, error) {
 
 	for _, paste := range pastes {
 		if paste.Expired() {
-			if tx := p.db.Delete(&Paste{}, paste.ID); tx.Error != nil {
-				return []Paste{}, tx.Error
+			if err := p.db.Delete(&Paste{}, paste.ID).Error; err != nil {
+				return []Paste{}, err
 			}
+		} else {
+			pastesRet = append(pastesRet, paste)
 		}
 	}
 
-	return pastes, nil
+	return pastesRet, nil
 }
