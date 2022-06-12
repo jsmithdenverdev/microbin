43a423e3e596ef0a6d1e7b64738efe40d29fd78a
Jacob Smith <jsmithdenverdev@gmail.com>
2022-06-11 22:31:38 -0600

    simplified errors

--- errors.go
+++ errors.go
@@ -2,20 +2,10 @@ package microbin
 
 import (
 	"errors"
-	"fmt"
 )
-
-type errFunc func() string
-
-func (e errFunc) Error() string {
-	return e()
-}
 
 var (
 	ErrInvalidExpiration = errors.New("invalid expiration value")
-	ErrExpiredPaste      = func(id int) error {
-		return errFunc(func() string {
-			return fmt.Sprintf("paste %d is expired", id)
-		})
-	}
+	ErrExpiredPaste      = errors.New("paste expired")
+	ErrPasteNotFound     = errors.New("not found")
 )
