fda64a046699366c8eecc0b294fc31f00ac98b70
Jacob Smith <jsmithdenverdev@gmail.com>
2022-08-21 13:15:09 -0600

    Removed duplicate error types

--- http/errors.go
+++ http/errors.go
@@ -1,32 +1,5 @@
 package http
 
-import (
-	"fmt"
-)
-
 const (
 	errorInternalServer = "Internal Server Error"
 )
-
-type (
-	errorPasteExpired struct {
-		ID int
-	}
-
-	errorUnrecognizedExpiration struct {
-		Expiration string
-	}
-)
-
-func (e errorPasteExpired) Error() string {
-	return fmt.Sprintf("paste %d expired", e.ID)
-}
-
-func (e errorUnrecognizedExpiration) Error() string {
-	if e.Expiration == "" {
-
-		return ("unrecognized expiration: none provided")
-	}
-
-	return fmt.Sprintf("unrecognized expiration: %s", e.Expiration)
-}
